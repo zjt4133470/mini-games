9623404bc55bab135d7de00002ea4d93419837ae
zjt4133470 <zjt4133470@163.com>
2019-05-29 17:55:36 +0800

    no message

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// 轮播图
+var mySwiper = new Swiper ('.swiper-container', {
+  loop: true, // 循环模式选项
+  // 如果需要前进后退按钮
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
